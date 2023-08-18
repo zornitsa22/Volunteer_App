46ca9784b0ebec9197ad850a3fe400c029235a71
RashmiMendpara <rashmimendpara2020@gmail.com>
2023-08-18 16:32:12 +0200

    Volunteer Authentication is done

---
+++ server/middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.JWT_SECRET;
+const authenticate = async (req, res, next) => {
+    console.log("auth.js is started........")
+  try {
+    console.log('🚀 ~ file: auth.js:6 ~ authenticate ~ accessToken:', req.cookies.accessToken);
+    if (req.cookies.accessToken) {
+      const volunteer = await jwt.verify(req.cookies.accessToken, SECRET);
+      req.volunteer = volunteer;
+      next();
+    } else {
+      res.status(403).json({ message: 'Forbidden' });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = authenticate;
