d70ed4439adee27fec2986c4a44d59d6c05e732c
Shelton <127335807+sheltoncoder@users.noreply.github.com>
2023-09-06 15:52:04 +0200

    render block

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
-require('dotenv/config');
-const cors = require('cors');
-const express = require('express');
-const cookieParser = require('cookie-parser');
+require("dotenv/config");
+const cors = require("cors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
-const connectDB = require('./config/db');
-
+const connectDB = require("./config/db");
 
 // Importing your routers
-const projectRouter = require('./routes/projects');
-const volunteerRouter = require('./routes/volunteers');
-const organizationRouter = require('./routes/organizations');
+const projectRouter = require("./routes/projects");
+const volunteerRouter = require("./routes/volunteers");
+const organizationRouter = require("./routes/organizations");
 
 // Import your routers for authentication
-const authRouter = require('./routes/auth-volunteer');
-const authRouterOrga = require('./routes/auth-organization');
+const authRouter = require("./routes/auth-volunteer");
+const authRouterOrga = require("./routes/auth-organization");
 
 // setting the portNumber
 const PORT = process.env.PORT || 8000;
@@ -26,17 +25,25 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Mounting authentication routers
-app.use('/auth', authRouter);
-app.use('/authOrga', authRouterOrga);
+app.use("/auth", authRouter);
+app.use("/authOrga", authRouterOrga);
 
 // Mounting volunteer/project/organization routers
-app.use('/api/projects', projectRouter);
-app.use('/api/volunteers', volunteerRouter);
-app.use('/api/organizations', organizationRouter);
+app.use("/api/projects", projectRouter);
+app.use("/api/volunteers", volunteerRouter);
+app.use("/api/organizations", organizationRouter);
 
+// DEPLOYMENT
+// this need to be after all routes
+if (process.env.NODE_ENV === "production") {
+  //Set static folder up in production
+  const buildPath = path.join(__dirname, "../client/dist");
+  app.use(express.static(buildPath));
+
+  app.get("", (req, res) => res.sendFile(path.join(buildPath, "index.html")));
+}
 // database connection and server starting
 connectDB().then(() => {
   console.log("Db connected");
   app.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));
 });
-
